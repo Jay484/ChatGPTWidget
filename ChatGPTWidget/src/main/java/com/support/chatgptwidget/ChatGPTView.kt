ba3e612ba747eb35ba6a3ef489ef94dcdc503341
dJay <djhinkwan09@gmail.com>
2023-01-29 10:07:27 +0800

    Initial commit

---
+++ ChatGPTWidget/src/main/java/com/support/chatgptwidget/ChatGPTView.kt
@@ -0,0 +1,21 @@
+package com.support.chatgptwidget
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.constraintlayout.widget.ConstraintLayout
+
+class ChatGPTView @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyleAttr: Int = 0,
+    defStyleRes: Int = 0
+): ConstraintLayout(context, attrs, defStyleAttr, defStyleRes){
+
+    init {
+        inflate(context, R.layout.chat_gpt_view_layout,this)
+    }
+
+    fun test(){
+
+    }
+}
